8fbf776bbdac151ae2869019bb9159fde9ac5a3e
Slimkin <doc.slimkin.dgi@google.com>
2020-05-30 18:42:26 +0300

    mod dec_v1 / add dec_v2

--- decorator_v1.py
+++ decorator_v1.py
@@ -5,10 +5,9 @@ def logger(old_function):
         start = str(datetime.now())
         result = old_function(*args, **kwargs)
         with open('log.txt', 'a', encoding='utf8' ) as f:
-            f.write(f'{start} - вызов функции\n{str(old_function.__name__)} - название функции\n{str(args)}{str(kwargs)} - аргументы функции\n{(result)} - результат функции\n\n')
+            f.write(f'{start} - вызов функции\n{str(old_function.__name__)} - название функции\n\
+{str(args)}{str(kwargs)} - аргументы функции\n{(result)} - результат функции\n\n')
         return result
-        with open('log.txt', 'a', encoding='utf8' ) as f:
-            f.write(f'{str(result)} - результат функции\n\n')
     return log
 
 
